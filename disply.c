897fc1f6373d25691b2eb86e9c6b7b5afa2a1878
emmylem <benjaminosondu55@gmail.com>
2022-11-14 21:31:16 +0000

    new

---
+++ disply.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+  Mydisply - the printf function
+  *integer - convert to number
+ */
+void Mydisply(char *str,...)
+{
+	char *s;
+	unsigned int i;
+	char *ptr;
+
+
